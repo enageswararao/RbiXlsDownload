934138b9e6e9f012a55d874f3dd6ff2ca42b05a3
Nageswar <enageswararao@gmail.com>
2020-04-26 12:29:47 +0530

    Change Path

--- src/test/java/com/example/JanSampleProject/RBIBankExcelDownload.java
+++ src/test/java/com/example/JanSampleProject/RBIBankExcelDownload.java
@@ -49,7 +49,10 @@ public class RBIBankExcelDownload {
     options.setExperimentalOption("prefs", preferences);
   // DesiredCapabilities capabilities = DesiredCapabilities.chrome();
   //  driver = new ChromeDriver(capabilities);
-    System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/workspace/RbiIfscXlsx/drivers/chromedriver");
+   String path = File.separator + "var"+ File.separator + "lib"+ File.separator + "jenkins"+ File.separator + "workspace"+ File.separator + "RbiIfscXlsx"+ File.separator + "drivers"+File.separator + "chromedriver";
+    System.setProperty("webdriver.chrome.driver",path);
+
+    //  System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/workspace/RbiIfscXlsx/drivers/chromedriver");
  //   WebDriverManager.chromedriver().operatingSystem(OperatingSystem.LINUX).setup();
 
 
